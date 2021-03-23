8e20b81ab314d8959cdb171375213bf52a628d13
Wallace Gonçalves <wallacecosme@gmail.com>
2021-03-23 16:10:42 -0300

    Configuração inicial para os testes

--- wallet/src/test/java/com/wallet/HelloWord.java
+++ wallet/src/test/java/com/wallet/HelloWord.java
@@ -1,15 +1,17 @@
 package com.wallet;
 
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class WalletApplicationTests {
-
+public class HelloWord {
+	
 	@Test
-	void contextLoads() {
+	public void testHelloWorld() {
+		assertEquals(1, 1);
 	}
 
 }
